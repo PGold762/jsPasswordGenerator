08b1434e9db3c5b75fed1043a8e1ee15ac7549ce
PGold <pglz762@gmail.com>
2023-07-18 08:27:26 -0500

    initial push of edited, but incomplete, files

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# jsPasswordGenerator
+# Challenge 3 - JS Password Generator
+
+## Description
+
+Employer's want to view my work history and this page will display the history in an easy to follow format.
+
+### User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+### Acceptance Criteria
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Installation
+
+Download the files from the repo and install on domain. Should work instantly.
+
+## Usage
+
+This is a responsive website that has a password generator for someone to use.
+
+Link to view: [My Profile Site](https://pgold762.github.io/mod2-profile/)
+
+Screenshot of website: 
+
+![Picture of My Profile site](./assets/images/screenshot-project2.png "My Profile Site")
+
+## Credits
+
+Used Google a lot for understanding definitions of different function and variable possibilities, like parseInt [Google](https://www.google.com/search?q=what+does+parseint+mean+in+js&rlz=1C1RXQR_enUS992US992&oq=&aqs=chrome.0.35i39i362l8.105735j0j7&sourceid=chrome&ie=UTF-8)
+
+Used the following to help write code for randommly generating password: [W3Schools] (https://www.w3schools.com/js/js_random.asp)
+
+
+
+## License
+
+MIT License
---
+++ script.js
@@ -0,0 +1,195 @@
+// Array of numbers to be in the password
+var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of special characters to be in the password
+var specialCharacters = [
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '-',
+  '_',
+  '+',
+  '}',
+  '{',
+  ']',
+  '[',
+  ':',
+  "'",
+  '?',
+  '/',
+  ',',
+  '.',
+  '~'
+];
+
+// Array of lower case letters to be in the password
+var lowerCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+// Array of capital letters to be in the password
+var upperCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+// Function for user to be prompt
+function passwordPrompt() {
+  // Variable to store password length
+    var passwordLength = parseInt(
+        prompt('Input the amount of characters you would like for the password'),
+        11
+    );
+
+    // Conditional to check for the length of the password being a number & prompts user if it is not
+    if (Number.isNaN(passwordLength)) {
+      alert('Length of password must be a number')
+      return null;
+    }
+
+    // Conditional to check if password is at least 8 characters long & prompts user if it is not
+    if (passwordLength < 8) {
+      alert('Password must be at least 8 characters')
+      return null;
+    }
+
+    // Conditional to check if password is 128 characters or less & prompts user if it is not
+    if (passwordLength > 128) {
+      alert('Password must be no more than 128 characters')
+      return null;
+    }
+
+    // Variable to store special characters
+    var storeSpecialCharacters = confirm(
+      'Click OK to accept using special characters.'
+    )
+
+    // Variable to store number characters
+    var storeNumberCharacters = confirm(
+      'Click OK to accept using numbers.'
+    )
+
+    // Variable to store lowercase letters
+    var storeLowercaseCharacters = confirm(
+      'Click OK to accept using lowercase letters'
+    )
+
+    // Variable to store capital letters
+    var storeUpperCharacters = confirm(
+      'Click OK to accept using captial letters.'
+    )
+
+
+    // Checks to ensure the user selected a character type
+    if (
+      storeSpecialCharacters === false &&
+      storeNumberCharacters === false &&
+      storeLowercaseCharacters === false &&
+      storeUpperCharacters === false
+    )
+    {
+      alert('You must select a character type')
+      return null;
+    }
+
+    //Variable to store the user's options
+    var passOptions = {
+      passwordLength: passwordLength,
+      storeUpperCharacters: storeUpperCharacters,
+      storeLowercaseCharacters: storeLowercaseCharacters,
+      storeNumberCharacters: storeNumberCharacters,
+      storeSpecialCharacters: storeSpecialCharacters,
+    };
+
+    return passwordPrompt;
+
+
+
+} // closing tag for passwordPrompt
+
+// Pick a random element for the password creation
+function passwordRandomizer() {
+  var randomizerMath = Math.floor(Math.random() * Array.passwordLength);
+  var randomizerItem = arr[randomizerEquation];
+
+  return randomizerItem;
+}
+
+// Function to create the password
+function passwordCreate() {
+ var passwordCreateOptions = passwordPrompt();
+ var passwordCreateItems = [];
+
+} //closing tag for passwordCreate
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = passwordCreate();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
